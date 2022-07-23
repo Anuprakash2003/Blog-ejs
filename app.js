0a825daa53ce0be7c9f05c4ef586eba4bd7ae994
unknown <anuprakash.cs20@bitsathy.ac.in>
2022-07-23 06:07:01 +0530

    challenge 16

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+let posts = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.render("home", { para: homeStartingContent });
+    res.render("home", { para: homeStartingContent, posts: posts });
+
 });
 app.get("/about", function(req, res) {
     res.render("about", { name: "about", para1: aboutContent });
@@ -32,11 +33,26 @@ app.get("/compose", function(req, res) {
     res.render("compose");
 });
 
+
+
 app.post("/compose", function(req, res) {
-    var post ={
-        title:req.body.postTitle,
-    content:req.body.postBody};
-    
+    var post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
+
+});
+app.get("/posts/:postName", function(req, res) {
+    let requestedVar = req.params.postName;
+    posts.forEach(function(post) {
+        let postedVar = post.title;
+        if (requestedVar === postedVar) {
+            console.log("match found");
+        }
+    });
+
 });
 app.listen(3100, function(req, res) {
     console.log("App started at port 3100");
