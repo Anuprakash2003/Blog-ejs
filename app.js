21d29878ac8edae0f96572a051acbd2b53285c2f
unknown <anuprakash.cs20@bitsathy.ac.in>
2022-07-24 19:02:13 +0530

    final Draft

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -45,11 +46,12 @@ app.post("/compose", function(req, res) {
 
 });
 app.get("/posts/:postName", function(req, res) {
-    let requestedVar = req.params.postName;
+    let requestedVar = _.lowerCase(req.params.postName);
     posts.forEach(function(post) {
-        let postedVar = post.title;
+        let postedVar = _.lowerCase(post.title);
         if (requestedVar === postedVar) {
-            console.log("match found");
+            console.log("success");
+            res.render("post", { title: post.title, content: post.content });
         }
     });
 
